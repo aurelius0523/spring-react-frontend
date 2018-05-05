6809c6e49f1c6bf8ff5849d0673727180c3e0ed7
Tan Kim Loong Ian <aurelius0523@gmail.com>
2018-05-05 18:10:31 +0800

    added babel.rc, updated npm script, added redux and its folders

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { format } from "url";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <form id="dummyFrom">
+        <label>A from!</label>
+        <input type="text" />
+      </form>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,40 @@
+const path = require("path");
+const SRC_DIR = path.resolve(__dirname, "src");
+const DIST_DIR = path.resolve(__dirname, "dist");
+const NODE_DIR = path.resolve(__dirname, "node_modules");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: './src/app.js',
-    devServer: {
-        contentBase: './dist/'
-    }
-}
+  entry: SRC_DIR + "/index.jsx",
+  devServer: {
+    contentBase: DIST_DIR
+  },
+  module: {
+    //loaders to use for different file types
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: NODE_DIR,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    path: DIST_DIR,
+    filename: "bundle.js"
+  },
+  //this allows extensions to be omitted when importing
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  //takes index.html at root and updates them with new hashed dependencies
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "index.html",
+      filename: "./index.html"
+    })
+  ]
+};
