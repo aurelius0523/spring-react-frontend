7de03c00c6d12eb2437fab5743f72bb44ca3578a
aurelius0523 <aurelius0523@gmail.com>
2018-05-10 23:20:30 +0800

    test out styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <header>This is Header</header>;
+const Header = styled.header`
+  width: 100%;
+  background-color: #20232a;
+  color: white;
+  padding: 2rem;
+  font-size: 2rem;
+`;
+
+export default () => <Header>This is Header</Header>;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { format } from "url";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import styled from "styled-components";
 import style from "../styles/main.css";
+const GridParent = styled.div`
+  display: grid;
+  margin: 0px;
+`;
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <GridParent>
         <Header />
         <div className="content" />
         <Footer />
-      </div>
+      </GridParent>
     );
   }
 }
